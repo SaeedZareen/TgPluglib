45189cb70e1425f03834d2435f46ee4eae0e1c8f
Saeed Zareen <40164278+SaeedZareen@users.noreply.github.com>
2018-06-11 09:01:56 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<title>TgPluglib - Telegram Plugin library</title>
+<center>
+  <h1>TgPluglib - Telegram Plugin library</h1>
+  <h2><a href="https://github.com/EdgeCO/TgPluglib">Repository on github</a></h2>
+  <h3><a href="https://t.me/emlnem">Developer</a></h3>
+  <h3><a href="https://t.me/edge_Bot">Support Bot</a></h3>
+  <h3><a href="https://t.me/edgeteam">Channel</a></h3>
