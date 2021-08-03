40d6b2db8f5ff4eaf6bd8df19aaeb43bb2cc8041
Saeed Zareen <40164278+SaeedZareen@users.noreply.github.com>
2021-08-03 09:34:15 +0430

    Just Remove SupportBot

--- index.php
+++ index.php
@@ -3,5 +3,4 @@
   <h1>TgPluglib - Telegram Plugin library</h1>
   <h2><a href="https://github.com/EdgeCO/TgPluglib">Repository on github</a></h2>
   <h3><a href="https://t.me/emlnem">Developer</a></h3>
-  <h3><a href="https://t.me/edge_Bot">Support Bot</a></h3>
   <h3><a href="https://t.me/edgeteam">Channel</a></h3>
